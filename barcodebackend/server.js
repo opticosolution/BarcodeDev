b27d8fbe5b8b1852b5d3cc519a4177af46f41515
jayshankar <jayshankaryadav435@gmail.com>
2025-06-04 15:01:21 +0530

    port change

--- barcodebackend/server.js
+++ barcodebackend/server.js
@@ -1422,8 +1422,12 @@ app.put('/settings/barcode-range', authenticateToken, checkRole(['admin', 'super
   }
 });
 
+app.get('/',(req,res)=>{
+  res.send("dev server running")
+})
+
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
